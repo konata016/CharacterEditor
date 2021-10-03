38ec637579ed7a872c6468fbd0a5746fd705ee29
konata016 <konata016@gmail.com>
2021-10-04 01:52:11 +0900

    Test

---
+++ Assets/My/Script/FaceItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FaceItem : MonoBehaviour
+{
+    [SerializeField]
+    private Image image;
+
+    [SerializeField]
+    private Button button;
+
+    public void SetImage(Sprite sprite)
+    {
+        image.sprite = sprite;
+    }
+
+    public void SetPos(Vector3 pos)
+    {
+        var rect = gameObject.transform as RectTransform;
+        rect.anchoredPosition = pos;
+    }
+
+    public void SetupButton(Action<Sprite> onClickButton)
+    {
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(() => onClickButton?.Invoke(image.sprite));
+    }
+}
---
+++ Assets/My/Script/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility : MonoBehaviour
+{
+    /// <summary>
+    /// リソース生成（GameObject）
+    /// </summary>
+    public static GameObject InstantiateResource(
+        string resourceName,
+        Transform parent = null)
+    {
+        var obj = Resources.Load(resourceName) as GameObject;
+        return Instantiate(obj, parent);
+    }
+}
